d251b35a46c20497a2a78510cd882b55c0f9b960
chenjl <chenjl@chenjl-ThinkPad.(none)>
2014-08-03 00:36:25 +0800

    add update

--- routes/index.js
+++ routes/index.js
@@ -7,14 +7,18 @@ router.get('/', function(req, res) {
 	console.log(req.query);
 	if(_.isEmpty(req.query)){
 		res.render('index', { title: 'Express' });
-    console.log("nothing");
+        console.log("nothing");
 	}else if(_.contains(_.keys(req.query), 'query')){
 		res.redirect("/query/" + req.query.query);
 	}else if(_.contains(_.keys(req.query), 'add_name') && _.contains(_.keys(req.query), 'add_value')){
-    res.redirect("/query/add/" + req.query.add_name + "/" + req.query.add_value);
-  }else if(_.contains(_.keys(req.query), 'del_name')){
-    res.redirect("/query/delete/" + req.query.del_name);
-  }
+        res.redirect("/query/add/" + req.query.add_name + "/" + req.query.add_value);
+    }else if(_.contains(_.keys(req.query), 'del_name')){
+        res.redirect("/query/delete/" + req.query.del_name);
+    }else if(_.contains(_.keys(req.query), 'update_name') && _.contains(_.keys(req.query), 'value')){ 
+        res.redirect("/query/update/" + req.query.update_name + "/" + req.query.value);
+    }else{
+        res.render('index', { title: 'Express' });
+    }
 });
 
 module.exports = router;
