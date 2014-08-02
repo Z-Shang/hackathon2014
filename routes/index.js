eb88380e110306da04371354400dde9fe2016f3b
CdiajadeX <chenyuxiaodhr@gmail.com>
2014-08-02 23:57:31 +0800

    add add & delete func

--- routes/index.js
+++ routes/index.js
@@ -7,9 +7,14 @@ router.get('/', function(req, res) {
 	console.log(req.query);
 	if(_.isEmpty(req.query)){
 		res.render('index', { title: 'Express' });
-	}else{
+    console.log("nothing");
+	}else if(_.contains(_.keys(req.query), 'query')){
 		res.redirect("/query/" + req.query.query);
-	}
+	}else if(_.contains(_.keys(req.query), 'add_name') && _.contains(_.keys(req.query), 'add_value')){
+    res.redirect("/query/add/" + req.query.add_name + "/" + req.query.add_value);
+  }else if(_.contains(_.keys(req.query), 'del_name')){
+    res.redirect("/query/delete/" + req.query.del_name);
+  }
 });
 
 module.exports = router;
